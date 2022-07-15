d81a139d02fd09fec654d263899b08d92ce12ae5
Matheus Reimer <matheusreimer1@gmail.com>
2022-07-14 23:12:17 -0300

    exercises page

--- MMP/components/Exercises/Exercises.tsx
+++ MMP/components/Exercises/Exercises.tsx
@@ -1,57 +1,138 @@
 import * as React from "react";
 import {
-  Button,
   View,
-  Text,
   Image,
-  Platform,
-  Keyboard,
-  TextInput,
+  TouchableOpacity,
 } from "react-native";
 import Sriracha from "../GeneralStyles/Sriracha";
 import QuickSand from "../GeneralStyles/Quicksand";
 import styles from "./Styles";
 import {
-  AntDesign,
-  Entypo,
   Feather,
-  Zocial,
   Ionicons,
-  MaterialIcons,
+  FontAwesome5,
 } from "@expo/vector-icons";
 import { Modal } from "../Modal";
-import { useState, useEffect } from "react";
-import Caveat from "../GeneralStyles/Caveat";
+import { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker";
+import { ScrollView } from "react-native-gesture-handler";
+
 
 export default function Exercises() {
   const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const [isExerciseListModalVisible, setExerciseListModalVisible] = React.useState(false);
 
-  useEffect(() => {
-    const checkForSubscription = setTimeout(() => {
-      setIsModalVisible(() => !isModalVisible);
-    }, 1500);
-    return () => clearTimeout(checkForSubscription);
-  }, []);
+  let savedItems;
+  let savedExercises;
+  let selectedWeekday_Index;
 
-  const handleSignUp = () => {
-    // sign up the user and close the modal
+  const openExerciseListModal = (index:number) =>{
+    //saving array first
+    savedExercises = [...selectedExercise]
+    
+    let selectedValue = selectedItems[index];
+    selectedWeekday_Index =items.map(({ value }) => value).indexOf(selectedValue)
+    setExerciseListModalVisible(() => !isExerciseListModalVisible)
+  }
+
+  const openModal = () => {
+    savedItems = [...selectedItems];
     setIsModalVisible(() => !isModalVisible);
   };
 
-  const handleDecline = () => setIsModalVisible(() => !isModalVisible);
+  const accept = () => {
+    savedItems = [...value];
+    setIsModalVisible(() => !isModalVisible);
+    setSelectedItems(savedItems);
+  };
+  const cancel = () => {
+    setIsModalVisible(() => !isModalVisible);
+  };
 
-  const handleModal = () => setIsModalVisible(() => !isModalVisible);
-  const [item, setItems] = useState<any>([]);
+  const acceptExercises = () => {
+    savedExercises = [...exerciseValues]
+    setExerciseListModalVisible(()=>!isExerciseListModalVisible)
+    setSelectedExercises(savedExercises)
+  }
+  const cancelExercise = () => {
+    setExerciseListModalVisible(()=>!isExerciseListModalVisible)
+  }
+ 
   const generateKey = (pre: { pre: any }) => {
     return `${pre}_${new Date().getTime()}`;
   };
 
-  /* 
-    function addItem() {
-        setItems([...item, <Text key={generateKey(item)}>aaaaa</Text>]);
-       
-    }
-    */
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    { label: "Monday", value: "Monday" },
+    { label: "Tuesday", value: "Tuesday" },
+    { label: "Wednesday", value: "Wednesday" },
+    { label: "Thursday", value: "Thursday" },
+    { label: "Friday", value: "Friday" },
+    { label: "Saturday", value: "Saturday" },
+    { label: "Sunday", value: "Sunday" },
+  ]);
+
+  const [selectedExercise, setSelectedExercises] = useState([])
+  const [openExerciseModal, setOpenExerciseModal] = useState(false);
+  const [exerciseValues, setExerciseValues] = useState([]);
+  const [exerciseItems, setExerciseItems] = useState([
+    { label: "Bench Press", value: "BenchPress" },
+  ]);
+
+  const listRemoval = (index: number) => {
+    let itemsCopy = [...value];
+    itemsCopy.splice(index, 1);
+    setValue(itemsCopy);
+  };
+
+  const cardsList = () => {
+    return selectedItems.map((element, index) => {
+      return (
+        <View style={styles.weekDayPlanContainer} key={generateKey(element)}>
+          <TouchableOpacity style={styles.weekDayPlan} onPress={()=>openExerciseListModal(index)}>
+            <View style={styles.indexBtn}><QuickSand color={"black"} flex={0} fontsize={20} text={index+1}></QuickSand></View>
+            <View><QuickSand color={"black"} flex={0} fontsize={20} text={element}></QuickSand></View>
+            <View style={styles.editBtn}><FontAwesome5 name="edit" size={20} color="black"/></View>
+          </TouchableOpacity>
+        </View>
+      );
+    });
+  };
+
+  const list = () => {
+    return value.map((element, index) => {
+      return (
+        <View style={styles.chosenExerciseContainer} key={generateKey(element)}>
+          <View style={styles.chosenExercisesModal}>
+            <View style={styles.index}>
+              <QuickSand
+                color={"black"}
+                flex={0}
+                fontsize={18}
+                text={index + 1}
+              ></QuickSand>
+            </View>
+            <View>
+              <QuickSand
+                color={"black"}
+                flex={0}
+                fontsize={18}
+                text={element}
+              ></QuickSand>
+            </View>
+          </View>
+          <View style={styles.deleteIcon}>
+            <TouchableOpacity onPress={() => listRemoval(index)}>
+              <Feather name="trash" size={20} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    });
+  };
 
   return (
     <View style={styles.mainContainer}>
@@ -80,53 +161,148 @@ export default function Exercises() {
               <QuickSand
                 color={"black"}
                 flex={0}
-                fontsize={15}
-                text={"Add another plan"}
+                fontsize={20}
+                text={"Add or remove another plan"}
               ></QuickSand>
               <Ionicons
                 name="add-circle-sharp"
                 size={34}
                 color="#128B3B"
-                onPress={handleModal}
+                onPress={openModal}
                 style={{ position: "absolute", bottom: -15 }}
               />
               <View style={styles.openedModal}>
-              <Modal isVisible={isModalVisible} >
-                <Modal.Container>
-                  <AntDesign
-                    name="minuscircleo"
-                    size={30}
-                    color="#128B3B"
-                    onPress={handleDecline}
-                    style={{ position: "absolute", top: 10, right: 10 }}
-                  />
-                  <AntDesign
-                    name="minuscircleo"
-                    size={30}
-                    color="#128B3B"
-                    onPress={handleSignUp}
-                    style={{ position: "absolute", bottom: 10 }}
-                  />
-                  <View style={{}}>
-                    <Modal.Header title={(<Sriracha color={"black"} fontsize={25} spacing={2} text={"One step away"}></Sriracha>)}/>
+                <Modal isVisible={isModalVisible}>
+                  <Modal.Container>
+                    <Modal.Header
+                      title={
+                        <QuickSand
+                          color={"black"}
+                          fontsize={25}
+                          text={"One step away"}
+                          flex={0}
+                        ></QuickSand>
+                      }
+                    />
                     <Modal.Body>
-                      <QuickSand color={"black"} flex={0} fontsize={16} text={"Select the day you want to create a plan"}></QuickSand>
-                      <TextInput
-                        style={{}}
-                        placeholder="email"
-                        keyboardType="email-address"
+                      <QuickSand
+                        color={"black"}
+                        flex={0}
+                        fontsize={16}
+                        text={"Select the day you want to create a plan"}
+                      ></QuickSand>
+                      <DropDownPicker
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                        theme="LIGHT"
+                        multiple={true}
+                        mode="SIMPLE"
+                        badgeDotColors={["black"]}
+                      />
+                    </Modal.Body>
+
+                    <ScrollView
+                      contentContainerStyle={styles.scrollViewModalBody}
+                    >
+                      {list()}
+                    </ScrollView>
+
+                    <Modal.Footer>
+                      <View style={styles.footerStyle}>
+                        <TouchableOpacity style={styles.cancelBtn} onPress={cancel}>
+                          <QuickSand
+                            color={"red"}
+                            flex={0}
+                            fontsize={20}
+                            text={"Cancel"}
+                          ></QuickSand>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                          style={styles.acceptBtn}
+                          onPress={accept}
+                        >
+                          <QuickSand
+                            color={"green"}
+                            flex={0}
+                            fontsize={20}
+                            text={"Accept"}
+                          ></QuickSand>
+                        </TouchableOpacity>
+                      </View>
+                    </Modal.Footer>
+                  </Modal.Container>
+                </Modal>
+
+
+
+                <Modal isVisible={isExerciseListModalVisible}>
+                  <Modal.Container>
+                   
+
+                    <Modal.Header
+                      title={
+                        <QuickSand
+                          color={"green"}
+                          fontsize={25}
+                          text={"Exercise List"}
+                          flex={0}
+                        ></QuickSand>
+                      }
+                    />
+                    <Modal.Body>
+                      <QuickSand
+                        color={"green"}
+                        flex={0}
+                        fontsize={16}
+                        text={"Select all your exercises"}
+                      ></QuickSand>
+                      <DropDownPicker
+                        open={openExerciseModal}
+                        value={exerciseValues}
+                        items={exerciseItems}
+                        setOpen={setOpenExerciseModal}
+                        setValue={setExerciseValues}
+                        setItems={setExerciseItems}
+                        theme="LIGHT"
+                        multiple={true}
+                        mode="SIMPLE"
+                        badgeDotColors={["black"]}
                       />
                     </Modal.Body>
                     <Modal.Footer>
-                      <View style={{}}></View>
+                      <View style={styles.footerStyle}>
+                        <TouchableOpacity style={styles.cancelBtn} onPress={cancelExercise}>
+                          <QuickSand
+                            color={"red"}
+                            flex={0}
+                            fontsize={20}
+                            text={"Cancel"}
+                          ></QuickSand>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                          style={styles.acceptBtn}
+                          onPress={acceptExercises}
+                        >
+                          <QuickSand
+                            color={"green"}
+                            flex={0}
+                            fontsize={20}
+                            text={"Accept"}
+                          ></QuickSand>
+                        </TouchableOpacity>
+                      </View>
                     </Modal.Footer>
-                  </View>
-                </Modal.Container>
-              </Modal>
+                  </Modal.Container>
+                </Modal>
               </View>
             </View>
           </View>
         </View>
+        <View style={styles.listedWorkouts}>{cardsList()}</View>
       </View>
     </View>
   );
--- MMP/components/Exercises/Styles.tsx
+++ MMP/components/Exercises/Styles.tsx
@@ -39,7 +39,7 @@ const styles = StyleSheet.create(
         height:"85%",
         borderTopLeftRadius:50,
         borderTopRightRadius:50,
-        backgroundColor:"#E7F5FF",
+        backgroundColor:"white",
         alignContent:"center",
     
     },
@@ -79,8 +79,156 @@ const styles = StyleSheet.create(
         height:"20%",
         justifyContent:"center",
     },
-    
-    
+    scrollViewModalBody:{
+        alignItems:"center",
+        justifyContent:"center",
+        
+    },
+    chosenExerciseContainer:{
+        width:"100%",
+        justifyContent:"space-between",
+        flexDirection:"row",
+
+    },
+    chosenExercisesModal:{
+        backgroundColor:"white",
+        width:"80%",
+        marginBottom:10,
+        paddingTop:15,
+        paddingBottom:15,
+   
+        borderBottomWidth:0.5,
+        borderRadius: 10,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
+        elevation:15,
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+              
+    },
+    index:{
+        position:"absolute",
+        left:15,
+    },
+    deleteIcon:{
+        backgroundColor:"white",
+        width:"15%",
+        marginBottom:10,
+        paddingTop:15,
+        paddingBottom:15,
+   
+        borderBottomWidth:0.5,
+        borderRadius: 10,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
+        elevation:15,
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+       
+    },
+    footerStyle:{ 
+        alignItems:"center",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        width:"90%",
+       
+    },
+    cancelBtn:{
+        backgroundColor:"white",
+        width:"45%",
+        
+        paddingTop:7,
+        paddingBottom:7,
+        
+        borderBottomWidth:0.5,
+        borderRadius: 10,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
+        elevation:15,
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+       
+    },
+    acceptBtn:{
+        backgroundColor:"white",
+        width:"45%",
+        
+        paddingTop:7,
+        paddingBottom:7,
+        
+        borderBottomWidth:0.5,
+        borderRadius: 10,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
+        elevation:15,
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+    },
+    listedWorkouts:{
+        width:"75%",
+        alignItems:"center",
+        justifyContent:"center",
+        alignSelf:'center',
+        marginTop:100,
+        height:"auto",
+    },
+    weekDayPlanContainer:{
+        width:"100%",
+        
+    },
+
+    weekDayPlan:{
+        width:"100%",
+        backgroundColor:"white",
+        paddingTop:10,
+        paddingBottom:10,
+        borderBottomWidth:0.5,
+        borderRadius: 10,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
+        elevation:15,
+        alignItems:"center",
+        justifyContent:"space-evenly",
+        flexDirection:"row",
+        marginTop:15,
+    },
+    editBtn:{
+        position:'absolute',
+        right:5,
+    },
+    indexBtn:{
+        position:"absolute",
+        left:5,
+    }
 
   });
 
